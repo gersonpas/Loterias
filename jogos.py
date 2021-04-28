3549acf17113d5fc6e1b3d8ac1296df1526f1e03
gersonpas <sobrinho.gerson@gmail.com>
2021-04-27 22:19:23 -0300

    Versão v1.2.270421

--- jogos.py
+++ jogos.py
@@ -1,5 +1,6 @@
-from random import randint, sample
+from random import randint, sample,choice
 import time
+
 print('\033[0;33m{:=^44}'.format('\033[0;33m LOTERIAS '))
 print(''' 
 [1] = QUINA
@@ -23,20 +24,20 @@ elif loto == 5:
     opção, init, fim, qdezInit, qdezFim  = 'lOTOMANIA', 0, 99, 50, 50
 elif loto == 6:
     opção, init, fim, qdezInit, qdezFim  = 'DIA DE SORTE', 1, 30, 7, 15
-    Mes =  sample(range(1, 12), 1)
-print('Você escolheu {}, Ótima escolha!'.format(opção))
-qdezenas = int(input('\033[0;33mEscolha um jogo entre {} e {} Dezenas\033[0;33m: '.format(qdezInit, qdezFim)))
+    Meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro',
+             'Novembro', 'Dezembro']
+    mesecolhido = choice(Meses) # escolhe um mês dentre os meses
+print(f'Você escolheu {opção}, Ótima escolha!')
+qdezenas = int(input(f'\033[0;33mEscolha um jogo entre {qdezInit} e {qdezFim} Dezenas\033[0;33m: '))
 while qdezenas < qdezInit or qdezenas > qdezFim:
     qdezenas = int(input('Entre com uma opção valida: '))
 print('-='*27)
-print('Muito bem! Você escolheu a {} com {} Dezenas!'.format(opção, qdezenas))
+print(f'Muito bem! Você escolheu  {opção} com {qdezenas} Dezenas!')
 print('-='*27)
-print('\033[0;34m-='*50)
-print('\033[0;33mSorteando os Números...')
-time.sleep(1)
+print('\033[0;34m-='*42)
+print('\033[0;33mSorteando as Dezenas...')
+time.sleep(3)
 dezenas = sorted(sample(range(init, fim),qdezenas))
-print('\033[0;34mAqui {} Dezenas da Sorte: {}'.format(qdezenas ,dezenas))
-print('O Mês escolhido foi: {} '.format(Mes) if loto == 6 else '-='*50)
-print('-='*50 if loto == 6 else '')
-
-
+print(f'\033[0;31m{qdezenas} Dezenas Sorteadas ==> {dezenas}')
+print(f'O Mês escolhido foi: {mesecolhido}' if loto == 6 else '-='*50)
+print('-='*42 if loto == 6 else '')
